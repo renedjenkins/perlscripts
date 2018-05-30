08535b7bff350a22ed0e55682637850e520d14fa
renedjenkins <renedjenkins@gmail.com>
2018-05-30 18:27:48 -0500

    Moved .git to actual perlscripts director and edited README.txt and reversepicturerename.pl file, testing edits with commit and sync to github

--- reversepicturerename.pl
+++ reversepicturerename.pl
@@ -1,8 +1,27 @@
 #! /usr/bin/perl -w
 # picture rename
+#reversepicturerename.pl - takes 3 arguments, example, if you just enter
+#perl ./reversepicturerename.pl
+#It will output following:
+#number of args < 3, ARGS >0
+#Example input: perl ~/perlscripts/reversepicturerename.pl billfeeder 1 "./*"
+#Example input: perl ~/perlscripts/reversepicturerename.pl bow 1 "./*"
+#Example input: perl ~/perlscripts/reversepicturerename.pl field 1 "./*"
+#Example input: perl ~/perlscripts/reversepicturerename.pl freefeeder 1 "./*"
+#Example input: perl ~/perlscripts/reversepicturerename.pl hill 1 "./*"
+#Exiting...
+#
+#It expects new prefix name number to start with and list of files to
+#operate on.
+#
+#With give input it will operate on the new file to the oldest renameing with
+#given prefix with incrementing number, example if you have 100 files in a
+#directory and want to rename with prefix foo it will start with newest and
+#rename foo00001.txt - foo00101.txt
+
 
 if ( @ARGV < 3 ) {
-    print "number of args < 2, ARGS >" . scalar @ARGV . "\n";
+    print "number of args < 3, ARGS >" . scalar @ARGV . "\n";
     print "Example input: perl ~/perlscripts/reversepicturerename.pl billfeeder 1 \"\.\/\*\" \n";
     print "Example input: perl ~/perlscripts/reversepicturerename.pl bow 1 \"\.\/\*\" \n";
     print "Example input: perl ~/perlscripts/reversepicturerename.pl field 1 \"\.\/\*\" \n";
